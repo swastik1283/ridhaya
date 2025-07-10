b99a1a7a9d698178490fc991d8da671f4665a256
Swastik Kotwal <swastik1283kot@gmail.com>
2025-07-10 10:15:02 +0530

    Updated Api Endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,34 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import userRouter from './routes/userRoute.js'
-import productRouter from './routes/productRoute.js'
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
+import router from './routes/pageroute.js';
+
+// config for app 
+const app = express();
+const port = process.env.PORT || 4000;
+
+// DB + Cloudinary Connect
+connectDB();
+connectCloudinary();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/pages', router); 
 
 
-//config for app 
-const app=express()
-const port=process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+app.get('/', (req, res) => {
+  res.send("API working");
+});
 
-//middleware
-
-
-app.use(express.json())
-app.use(cors())
-
-
-//api endpoint
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.get('/',(req,res)=>{
-    res.send("Api working")
-})
-app.listen(port,()=>console.log("server started on port :"+port));
+app.listen(port, () => console.log("✅ Server started on port: " + port));
