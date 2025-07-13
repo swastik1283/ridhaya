79865eabcb1af1d794a2e4b70bc5d43eacd8c34b
Swastik Kotwal <swastik1283kot@gmail.com>
2025-07-13 13:31:09 +0530

    Improved display of carousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -30,7 +30,7 @@ const ProductCarousel = () => {
            
        <div className='mt-5'>
       <div className=' flex justify-between text-3xl text-black '>
-        <div className='text-center w-full'>
+        <div className='text-center font-semibold w-full'>
         Our Range of  Products
         </div>
       </div>
@@ -55,19 +55,19 @@ const ProductCarousel = () => {
 {products.map((product,idx)=>(
   <SwiperSlide key={product._id}>
   
-    <div className="max-w-sm max-w-lg w-full mt-5 rounded-xl overflow-hidden shadow-lg bg-white border border-gray-200">
+    <div className="max-w-sm max-w-lg w-full mt-5 rounded-xl overflow-hidden shadow-lg bg-white border border-gray-200  flex flex-col h-[400px]">
     <img
-  className='w-full h-48 object-cover'
+  className='w-full h-[180px] object-cover'
   src={product.image?.[0] || 'https://via.placeholder.com/300x200?text=No+Image'}
   alt={product.name}
 />
 
-      <div className='p-5'>
+      <div className='p-5 flex-1 flex flex-col'>
         <h2 className='text-xl font-bold mb-2'>
           {product.name}
         </h2>
-        <p className='text-gray-700 mb-4'>{product.description}</p>
-        <div className='flex justify-between items-center'>
+        <p className='text-gray-700 mb-4 text-sm h-[48px]'>{product.description.length>50 ? product.description.slice(0,50)+'...':product.description}</p>
+        <div className='flex justify-between items-center mt-auto pt-3'>
           <span className='text-lg font-semibold text-green-600'>₹{product.price}</span>
           <Link to={`/product/${product._id}`} className='bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800'>
   View Details
