9d1f49344cd56a9c3f9ab2cd91f5f5080e1d0ede
Swastik Kotwal <swastik1283kot@gmail.com>
2025-07-13 13:13:29 +0530

    Updated Sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -26,6 +26,17 @@ const Sidebar = () => {
             <img  className='w-5 h-5' src={assets.add_icon} alt=""/>
             <p className='hidden md:block'>Add Page</p>
             </NavLink>
+
+
+
+            <NavLink  className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l'to="/Carousel/add">
+            <img  className='w-5 h-5' src={assets.add_icon} alt=""/>
+            <p className='hidden md:block'>Add Carousel</p>
+            </NavLink>
+            <NavLink  className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l'to="/Carousel/list">
+            <img  className='w-5 h-5' src={assets.order_icon} alt=""/>
+            <p className='hidden md:block'> Carousel List</p>
+            </NavLink>
         </div>
     </div>
   )
