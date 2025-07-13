13e5eeaa9442676c658908a9355ec2a3acc7e5de
Swastik Kotwal <swastik1283kot@gmail.com>
2025-07-13 13:35:51 +0530

    Cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 const Cart = () => {
   const { cartItems, products, BackendUrl, currency, updateQuantity } = useContext(ShopContext);
   const [cartList, setCartList] = useState([]);
+  const Navigate=useNavigate();
 
   useEffect(() => {
     // Filter products to match items in cart
@@ -30,8 +31,8 @@ const Cart = () => {
           {cartList.map((item, index) => {
             const qty = cartItems[item._id || item.id];
             return (
-              <div key={index} className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] gap-4 items-center border p-2 rounded shadow-sm'>
-                <img src={item.image} className='w-16 h-16 object-cover rounded' alt={item.name} />
+              <div key={index} className='grid grid-cols-[1fr_2fr_1fr] md:grid-cols-[1fr_2fr_1fr_1fr_1fr_1fr] gap-4 items-center border p-2 rounded shadow-sm'>
+                <img src={item.image[0]} className='w-16 h-16 object-cover rounded' alt={item.name} />
                 <div>
                   <h3 className='font-semibold'>{item.name}</h3>
                   <p className='text-gray-600 text-sm'>{currency}{item.price}</p>
@@ -54,14 +55,13 @@ const Cart = () => {
                 >
                   Remove
                 </button>
-
-{/*                  
-                <button
-                  onClick={() => updateQuantity(item._id || item.id, 0)}
-                  className='text-red-500 text-sm hover:underline'
-                >
-                 Checkout
+                {/* <button onClick={Navigate('/Checkout')}>
+                CheckOut
                 </button> */}
+
+                 
+                
+
               </div>
             );
           })}
@@ -71,6 +71,25 @@ const Cart = () => {
       {cartList.length > 0 && (
         <div className='mt-6 text-right font-semibold text-xl'>
           Total: {currency}{getTotalPrice().toFixed(2)}
+       <button
+  onClick={() =>
+    Navigate('/checkout', {
+      state: {
+        cart: cartList.map((item) => ({
+          name: item.name,
+          price: item.price,
+          quantity: cartItems[item._id || item.id],
+          image: item.image,
+        })),
+        total: getTotalPrice(),
+      },
+    })
+  }
+  className="bg-green-600 text-white px-4 py-1.5 rounded-md shadow hover:bg-green-700 transition-all duration-200 text-sm"
+>
+  Checkout
+</button>
+
         </div>
       )}
     </div>
